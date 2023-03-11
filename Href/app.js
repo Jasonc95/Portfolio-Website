4b6301005ba87a137d326187ad3aa87d2b859d5c
Jasonc95 <jasonclough95@gmail.com>
2023-03-11 18:07:53 +0000

    test-2

--- Href/app.js
+++ Href/app.js
@@ -13,3 +13,22 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenSections = document.querySelectorAll('.hidden');
 hiddenSections.forEach((section) => observer.observe(section));
+
+
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const target = document.querySelector(link.hash.toLowerCase());
+    const targetOffset = target.offsetTop;
+    const windowHeight = window.innerHeight;
+    const targetHeight = target.offsetHeight;
+    const offset = targetOffset - (windowHeight - targetHeight + paddingTop) / 2;
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth'
+    });
+  });
+});
